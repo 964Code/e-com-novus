13dcd678925bbc8602d5a27f6359a4ed9898f526
Filip C <964code@gmail.com>
2023-04-22 16:10:23 +0200

    basic restenter ID here API for Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,32 +10,58 @@ const Product = require('../models/productModel');
 // @route  GET /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-    if (!req.body.text) {
-        res.status(400)
-        throw new Error('Please provide a text')
-    }
+    const products = await Product.find();
 
-    res.status(200).json({ message: 'Get products' });
+    res.status(200).json(products);
 });
 
 // @desc   CREATE a product
 // @route  POST /api/products
 // @access Public
 const createProduct = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Created product' });
+    if (!req.body.title || !req.body.description || !req.body.price || !req.body.image || !req.body.category) {
+        res.status(400);
+        throw new Error(`Please provide all the required fields. Title, description, price, image and category are required.`);
+    }
+
+    const product = await Product.create(req.body);
+
+    res.status(200).json(product);
 });
 
 // @desc   UPDATE a product
 // @route  PUT /api/products/:id
 // @access Public
 const updateProduct = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Updated product with ID: ${req.params.id}` });
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        res.status(404);
+        throw new Error(`Product with ID: ${req.params.id} not found.`);
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+
+
+    res.status(200).json(updatedProduct);
 });
 
 // @desc   DELETE a product
 // @route  DELETE /api/products/:id
 // @access Public
 const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        res.status(404);
+        throw new Error(`Product with ID: ${req.params.id} not found.`);
+    }
+
+    await product.deleteOne({ _id: req.params.id });
+
     res.status(200).json({ message: `Deleted product with ID: ${req.params.id}` });
 });
 
