27de66053aea4666c689c6ecb6d51cdc3d5d6a7b
Filip C <964code@gmail.com>
2023-04-25 19:57:34 +0200

    added category-picker on products-page

--- frontend/src/components/CategoriesMenu/CategoriesMenu.jsx
+++ frontend/src/components/CategoriesMenu/CategoriesMenu.jsx
@@ -1,8 +1,99 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import * as S from './styled';
+import { useNavigate } from 'react-router-dom';
+import { categoryData } from '../../data/categories/categories';
+
+const CategoriesMenu = () => {
+  const navigate = useNavigate();
+  const categoriesObj = categoryData;
+
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showSubcategories, setShowSubcategories] = useState(false);
+
+  const containerRef = useRef(null);
+
+  const handleCategoryClick = (category) => {
+    if (category === selectedCategory) {
+      setShowSubcategories(!showSubcategories);
+    } else {
+      setSelectedCategory(category);
+      setShowSubcategories(true);
+    }
+  };
+
+  const handleSubcategoryClick = (subcategory) => {
+    navigate(
+      `/products/${selectedCategory.toLowerCase()}/${subcategory.toLowerCase()}`
+    );
+  };
+
+  const handleClickOutside = (event) => {
+    if (containerRef.current && !containerRef.current.contains(event.target)) {
+      setShowSubcategories(false);
+      setSelectedCategory(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <>
+      <S.CategoriesMenuContainer ref={containerRef}>
+        <S.CategoriesMenuList>
+          {Object.entries(categoriesObj).map(([key, value]) => (
+            <S.CategoriesMenuItem key={key}>
+              <S.CategoriesMenuLink
+                onClick={() => {
+                  if (selectedCategory === key) {
+                    setShowSubcategories(!showSubcategories);
+                  } else {
+                    setSelectedCategory(key);
+                    setShowSubcategories(true);
+                  }
+                }}
+                active={selectedCategory === key}
+              >
+                {value.name}
+              </S.CategoriesMenuLink>
+              {showSubcategories &&
+                selectedCategory === key &&
+                value.subcategories && (
+                  <S.CategoriesSubMenuList>
+                    {Object.values(value.subcategories).map((subcategory) => (
+                      <S.CategoriesMenuItem key={subcategory.name}>
+                        <S.CategoriesSubMenuLink
+                          onClick={() =>
+                            handleSubcategoryClick(subcategory.name)
+                          }
+                        >
+                          {subcategory.name}
+                        </S.CategoriesSubMenuLink>
+                      </S.CategoriesMenuItem>
+                    ))}
+                  </S.CategoriesSubMenuList>
+                )}
+            </S.CategoriesMenuItem>
+          ))}
+        </S.CategoriesMenuList>
+      </S.CategoriesMenuContainer>
+    </>
+  );
+};
+
+export default CategoriesMenu;
+
+/* import React from 'react';
+import * as S from './styled';
+import { useNavigate } from 'react-router-dom';
 
 function CategoriesMenu() {
-  const categories = ['All', 'Electronics', 'Jewelery'];
+  const navigate = useNavigate();
+  const categories = ['All', 'Electronics', 'Fashion', 'Home'];
 
   return (
     <>
@@ -10,13 +101,21 @@ function CategoriesMenu() {
         <S.CategoriesMenuList>
           {categories.map((category) => (
             <S.CategoriesMenuItem key={category}>
-              <S.CategoriesMenuLink href='#'>{category}</S.CategoriesMenuLink>
+              <S.CategoriesMenuLink
+                onClick={() => {
+                  navigate(`/products/${category.toLowerCase()}`);
+                }}
+              >
+                {category}
+              </S.CategoriesMenuLink>
             </S.CategoriesMenuItem>
           ))}
         </S.CategoriesMenuList>
       </S.CategoriesMenuContainer>
+      ;
     </>
   );
 }
 
 export default CategoriesMenu;
+ */
---
+++ frontend/src/data/categories/categories.js
@@ -0,0 +1,44 @@
+export const categoryData = {
+    electronics: {
+        name: 'Electronics',
+        subcategories: {
+            phones: {
+                name: 'Phones',
+            },
+            laptops: {
+                name: 'Laptops',
+            },
+            tv: {
+                name: 'TV',
+            },
+        },
+    },
+    fashion: {
+        name: 'Fashion',
+        subcategories: {
+            mens: {
+                name: 'Mens',
+            },
+            womens: {
+                name: 'Womens',
+            },
+            kids: {
+                name: 'Kids',
+            },
+        },
+    },
+    home: {
+        name: 'Home',
+        subcategories: {
+            furniture: {
+                name: 'Furniture',
+            },
+            appliances: {
+                name: 'Appliances',
+            },
+            decor: {
+                name: 'Decor',
+            },
+        },
+    },
+};
