a758e336c463b304cef82248d2b6857ba33f5b62
Filip C <964code@gmail.com>
2023-05-03 20:15:55 +0200

    sorting for price high/low added.

--- frontend/src/pages/Products/Products.jsx
+++ frontend/src/pages/Products/Products.jsx
@@ -1,11 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import * as React from 'react';
-import {
-  fetchProducts,
-  selectProducts,
-  deleteProduct,
-} from '../../features/products/productSlice';
-import { Link } from 'react-router-dom';
+import { selectProducts } from '../../features/products/productSlice';
 import ProductList from '../../components/ProductList/ProductList';
 import CategoriesMenu from '../../components/CategoriesMenu/CategoriesMenu';
 import SearchBar from '../../components/Searchbar/Searchbar';
@@ -15,13 +10,14 @@ function Products() {
   const products = useSelector(selectProducts);
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = React.useState('');
+  const [sortOrder, setSortOrder] = React.useState('lowToHigh');
 
   // Return loading if products are not fetched
   if (products.length === 0) {
     return <h1>Loading...</h1>;
   }
 
-  let filteredProducts = products;
+  let filteredProducts = [...products];
 
   if (searchTerm.length > 0 && products.length > 0) {
     filteredProducts = products.filter((product) => {
@@ -29,11 +25,27 @@ function Products() {
     });
   }
 
+  if (sortOrder === 'lowToHigh') {
+    filteredProducts.sort((a, b) => a.price - b.price);
+  } else if (sortOrder === 'highToLow') {
+    filteredProducts.sort((a, b) => b.price - a.price);
+  }
+
   return (
     <>
       <S.Container>
         <CategoriesMenu />
         <SearchBar onSearch={setSearchTerm} />
+        <S.SortContainer>
+          <S.SortLabel>Sort by price:</S.SortLabel>
+          <S.SortSelect
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <option value='lowToHigh'>Low to high</option>
+            <option value='highToLow'>High to low</option>
+          </S.SortSelect>
+        </S.SortContainer>
         <ProductList products={filteredProducts} />
       </S.Container>
     </>
--- frontend/src/pages/Products/styled.jsx
+++ frontend/src/pages/Products/styled.jsx
@@ -4,5 +4,35 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px 20px;
+  margin: 20px auto;
+  padding: 0 20px;
+  width: 100%;
+  max-width: 1200px;
+`;
+
+export const SortContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 10px 0;
+`;
+
+export const SortLabel = styled.label`
+  font-size: 1rem;
+  margin-right: 10px;
+`;
+
+export const SortSelect = styled.select`
+  font-size: 1rem;
+  padding: 5px;
+  border: none;
+  border-radius: 5px;
+  background-color: #f7f7f7;
+  color: #333;
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    background-color: #eee;
+    outline: none;
+  }
 `;
